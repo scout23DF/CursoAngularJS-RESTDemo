b217282a24b197ed4c80a48eba5601c563793393
AndreNascimento <scout23DF@gmail.com>
2016-12-16 11:06:50 -0200

    Inclusão da verificação do Token de Autenticação (Auth / JWT)

--- app.js
+++ app.js
@@ -40,21 +40,8 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 // // Add headers
-app.use(function(req, res, next) {
-
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    } else {
-        next();
-    }
-});
 
 app.use('/', routes);
 app.use('/login', login);
